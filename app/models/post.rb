745dc643dd4d5bee10b5c4cd24a3b6a993db7562
Caroline Setiawan <vc.setiawan@gmail.com>
2019-10-22 13:35:35 +1100

    Added CRUD and categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :category_id
-  validates :title, :content, presence: true
+  validates :title, :content, :category, presence: true
+  has_many :line_items, inverse_of: :order
 end
